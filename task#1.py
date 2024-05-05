a41d4a7c06967a5532612f93bd5378a31dc4e422
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-05-05 18:16:42 +0300

    added task 1 and 2

---
+++ task#1.py
@@ -0,0 +1,20 @@
+def total_salary(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            salaries = []
+            for line in file:
+                _, salary= line.strip().split(',')
+                salary = int(salary)
+                salaries.append(salary)
+            total = sum(salaries)
+            average = total / len(salaries)
+            return (total, average)
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return (0, 0)
+    except Exception:
+        print(f"Сталася помилка.")
+        return (0, 0)
+
+total, average = total_salary("task#1.txt")
+print(total, average)
---
+++ task#2.py
@@ -0,0 +1,17 @@
+def get_cats_info(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            cats_info = []
+            for line in file:
+                id, name, age = line.strip().split(',')
+                cats_info.append({"id": id, "name": name, "age": age})
+            return cats_info
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return []
+    except Exception:
+        print("Сталася помилка.")
+        return []
+
+cats_info = get_cats_info("task#2.txt")
+print(cats_info)
